f685f81361043d4a10902103f77563d230f65923
Sreenadh S <sreeavtar@gmail.com>
2018-03-20 11:24:38 +0530

    Added functionality to validate account number and minimum balance

---
+++ lib/src/test/java/AccountTest.java
@@ -0,0 +1,38 @@
+import com.thoughtworks.bank.Account.Account;
+import com.thoughtworks.bank.Account.InvalidAccountNumberException;
+import com.thoughtworks.bank.Account.MinimumBalanceException;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+
+public class AccountTest {
+
+    private Account account;
+
+    @Before
+    public void setUp() throws MinimumBalanceException, InvalidAccountNumberException {
+        account = new Account("1234-5678", 2000);
+    }
+
+
+    @Test
+    public void checkBalance() {
+        assertThat(account.getBalance(), is(2000.0));
+    }
+    @Test
+    public void checkAccountNumber() {
+        assertThat(account.getAccountNumber(), is("1234-5678"));
+    }
+    @Test(expected = MinimumBalanceException.class)
+    public void checkMinimumBalance() throws MinimumBalanceException, InvalidAccountNumberException {
+        new Account("1234-4563",200);
+    }
+
+    @Test(expected = InvalidAccountNumberException.class)
+    public void validateAccountNumber() throws MinimumBalanceException, InvalidAccountNumberException {
+        new Account("123-1233",3000);
+    }
+}
